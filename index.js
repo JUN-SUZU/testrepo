431fd92b67ee95d0921c5ce29915c537a2c10ad8
JUN-SUZU <77867925+JUN-SUZU@users.noreply.github.com>
2023-09-29 16:36:12 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -14,12 +14,7 @@ let deviceType = getDeviceType();
 
 
 window.addEventListener('load', function() {
-    // device typeによって表示サイズとレイアウトを変更
-    if (deviceType == 'smartphone') {
-        document.getElementById('headerpt').style.display = 'none';
-        document.getElementsByTagName('main')[0].style.width = '100%';
-        Array.from(document.getElementsByTagName('article')).forEach(element => {
-            element.style.width = '80%';
-        });
-    }
+    // デバイスによって並びは同じにする(Cumulative Layout Shift対策)
+
+    // 速度が速い場合かつ従量課金制でないかつ高解像度の場合は2秒後に画像を高解像度に置き換える
 });
